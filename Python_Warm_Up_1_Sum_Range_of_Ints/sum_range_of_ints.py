a44710bbfb715f2abfdaf4e1ed64329e5b6284fd
Joseph Tallen <jtallen@hawk.iit.edu>
2021-05-26 23:09:39 -0500

    Created an exception handler for add_it_up

--- Python_Warm_Up_1_Sum_Range_of_Ints/sum_range_of_ints.py
+++ Python_Warm_Up_1_Sum_Range_of_Ints/sum_range_of_ints.py
@@ -3,16 +3,18 @@
 # - input and returns the sum of the integers from zero to the input parameter.
 # - The function should return 0 if a non-integer is passed in.
 def add_it_up(last_num):
-    i = 0
-    sumedup = 0
-    if not(isinstance(last_num,int)):
+    try:
+        i = 0
+        sumedup = 0
+        if not(isinstance(last_num,int)):
+            return sumedup
+        while i<last_num+1:
+            sumedup = sumedup + i
+            i += 1
         return sumedup
-    while i<last_num+1:
-        sumedup = sumedup + i
-        i += 1
-    return sumedup
+    except:
+        return "An Error Occured"+sys.exc_info()[0]
 print(add_it_up("a"))
 print(add_it_up("5"))
 print(add_it_up(5))
-print(add_it_up(0))
-print(add_it_up(i))
+print(add_it_up(100))
